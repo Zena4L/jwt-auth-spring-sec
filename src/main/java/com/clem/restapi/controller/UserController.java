aa41d6e1ac247b2faf91706fb4499545ab2410a2
Clement Owireku-Bogyah <zenerbogyah7@gmail.com>
2023-12-07 17:58:56 +0000

    feat:get all users

--- src/main/java/com/clem/restapi/controller/UserController.java
+++ src/main/java/com/clem/restapi/controller/UserController.java
@@ -2,13 +2,14 @@ package com.clem.restapi.controller;
 
 import com.clem.restapi.dto.AuthDto;
 import com.clem.restapi.dto.UserDto;
+import com.clem.restapi.model.User;
+import com.clem.restapi.repository.UserRepository;
 import com.clem.restapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService service;
+    private final UserRepository repository;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody UserDto request){
@@ -26,5 +28,10 @@ public class UserController {
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthDto request){
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUser(){
+        return repository.findAll();
     }
 }
